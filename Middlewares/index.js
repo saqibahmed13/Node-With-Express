7f50f879099086f4aab3fde1facaad89087d27ca
saqibahmed13 <saqiba0484@gmail.com>
2024-03-28 10:32:21 +0530

    added

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -2,11 +2,23 @@ const express = require( 'express' );
 const app = express()
 
 app.get('/checkup', function(req,res){
-    const username = req.header.username;
-    const password = req.header.password;
+    const username = req.headers.username;
+    const password = req.headers.password;
     const kidneyID = req.query.kidneyID;
-})
 
+    if(username!= "Saqib" || password!= "pass"){
+        res.status(400).json("Invalid Credentials")
+       
+    }
+
+    if(kidneyID!=1 && kidneyID!=2){
+        res.status(400).json("Invalid Kidney ID");
+        
+    }
+
+    res.json("you are all set");
+    
+});
 
 
 
