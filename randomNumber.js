b535eb668932409f0c32b4dd6976de6bed2885af
saqibahmed13 <saqiba0484@gmail.com>
2024-04-08 15:54:42 +0530

    ad

---
+++ randomNumber.js
@@ -0,0 +1,5 @@
+const phoneNumber = Math.floor(Math.random() * 900000000) + 100000000;;
+console.log("9"+ phoneNumber);
+
+const randomNumber = Math.floor(Math.random() * 10 );
+console.log(randomNumber)
